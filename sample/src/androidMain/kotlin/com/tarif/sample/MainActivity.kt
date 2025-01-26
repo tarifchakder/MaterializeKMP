d54a0dfe1313bc3d615e6774f355ec13c75ed45a
Tarif <tarifchakdar@gmail.com>
2025-01-27 00:20:31 +0530

    implement sample library UI for common

--- sample/src/androidMain/kotlin/com/tarif/sample/MainActivity.kt
+++ sample/src/androidMain/kotlin/com/tarif/sample/MainActivity.kt
@@ -3,21 +3,16 @@ package com.tarif.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             App()
+            SystemAppearance(isSystemInDarkTheme())
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
---
+++ sample/src/androidMain/kotlin/com/tarif/sample/SystemAppearance.kt
@@ -0,0 +1,25 @@
+package com.tarif.sample
+
+import android.app.Activity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun SystemAppearance(isDark: Boolean) {
+    val view = LocalView.current
+    val systemBarColor = MaterialTheme.colorScheme.surface.toArgb()
+    LaunchedEffect(!isDark) {
+        val window = (view.context as Activity).window
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = systemBarColor
+        window.navigationBarColor = systemBarColor
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = !isDark
+            isAppearanceLightNavigationBars = !isDark
+        }
+    }
+}
