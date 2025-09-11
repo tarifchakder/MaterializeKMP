f2cf626de3dcbc659880a6720feda6a58e9c8dc2
tarif <tarifchakdar@gmail.com>
2025-09-11 22:55:53 +0400

    Build: Disable abortOnError in lint options
    This commit updates the `build.gradle.kts` files for the `material-theme-kit` and `sample` modules.
    
    - Added `lint { abortOnError = false }` to the Android configuration block in both modules to prevent lint errors from halting the build.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -100,6 +100,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    lint {
+        abortOnError = false
+    }
 }
 
 dependencies {
