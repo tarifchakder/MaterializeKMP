daf0b9a5d526ecf39a2d6506b4773665ffd8ef56
tarif <tarifchakdar@gmail.com>
2025-09-12 22:54:11 +0400

    Build: Configure Sonatype OSSRH repository for publishing
    This commit updates the `material-theme-kit/build.gradle.kts` file.
    
    - Added a `maven` repository block within the `publishing` configuration.
        - Set the `url` to `https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/`.
        - Configured credentials to use `MAVEN_CENTRAL_USERNAME` and `MAVEN_CENTRAL_PASSWORD` environment variables for `username` and `password` respectively.

--- material-theme-kit/build.gradle.kts
+++ material-theme-kit/build.gradle.kts
@@ -159,6 +159,7 @@ publishing {
         }
         repositories {
             maven {
+                name = "sonatype"
                 url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
                 credentials {
                     username = System.getenv("MAVEN_CENTRAL_USERNAME")
