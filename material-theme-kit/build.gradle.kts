f861042d0fb6bffd1b919346e53b233a28a6faa4
tarif <tarifchakdar@gmail.com>
2025-09-13 09:36:40 +0400

    Refactor: Enable automatic publishing to Maven Central
    This commit updates the `material-theme-kit/build.gradle.kts` file.
    
    - Modified the `publishToMavenCentral` call within the `mavenPublishing` block to include `SonatypeHost.CENTRAL_PORTAL` and set `true` for automatic publishing.
    - Imported `com.vanniktech.maven.publish.SonatypeHost`.

--- material-theme-kit/build.gradle.kts
+++ material-theme-kit/build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import org.jetbrains.kotlin.load.kotlin.signatures
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
